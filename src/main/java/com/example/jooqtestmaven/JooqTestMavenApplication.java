903671a303583226aac9e129be0bf6a7741d0ea1
swucs <swucs7@gmail.com>
2022-06-23 17:40:31 +0900

    first commit!!

---
+++ src/main/java/com/example/jooqtestmaven/JooqTestMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.jooqtestmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqTestMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqTestMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jooqtestmaven/JooqTestMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jooqtestmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqTestMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
